4f541bf090efc0bfe749dca074f1d7cf4881c9cd
Mira B <mirabishh@gmail.com>
2023-12-28 18:27:46 -0500

    modified and tested and passed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   // if the string can't be reversed in place you have to change the string to an array then reverse the array and then return it as a string h e l l o -> iterate backwards and make a string "o l l e h" 
+   let arr = [];
+   for (let i = 0; i < word.length; i++) {
+      arr.push(word.charAt(i));
+    } //end for 
+    //console.log(arr);
+    newWord = "";
+    for (let index = arr.length - 1; index >= 0; index--) {
+      const element = arr[index];
+      newWord+= element;
+   }//end for 
+   //console.log(newWord)
+   return newWord
 };
-
+//reverseString("hello"); 
 // Do not edit below this line
 module.exports = reverseString;
