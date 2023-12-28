de1dba701a631bbbfc8740f7486ed4fd335df765
Mira B <mirabishh@gmail.com>
2023-12-28 18:12:46 -0500

    modified and passed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+   if (num < 0) {
+      return "ERROR"
+   }
+   let output = "";
+   let i = 0;
+   while (i < num) {
+      output += word;
+      i++;
+   }
+   return output
 };
 
 // Do not edit below this line
